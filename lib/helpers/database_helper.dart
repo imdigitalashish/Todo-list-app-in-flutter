4f822057e8381c8b990cd9c9d1d275dce00c5cc2
imdigitalashish <imdigitalashish@gmail.com>
2020-10-27 13:10:54 +0530

    Database Connection Pending

---
+++ lib/helpers/database_helper.dart
@@ -0,0 +1,55 @@
+import 'dart:io';
+
+import 'package:app/models/task_model.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:sqflite/sqflite.dart';
+
+class DatabaseHelper {
+  static final DatabaseHelper instance = DatabaseHelper._instance();
+  static Database _db;
+
+  DatabaseHelper._instance();
+
+  String tasksTable = "task_table";
+  String colId = "id";
+  String colTitle = "title";
+  String colDate = "date";
+  String colPriority = "priority";
+  String colStatus = "status";
+
+  Future<Database> get db async {
+    if (db == null) {
+      _db = await _initDb();
+    }
+    return _db;
+  }
+
+  Future<Database> _initDb() async {
+    Directory dir = await getApplicationDocumentsDirectory();
+    String path = dir.path + "todo_list.db";
+    final todoListDb =
+        await openDatabase(path, version: 1, onCreate: _createDb);
+    return todoListDb;
+  }
+
+  void _createDb(Database db, int version) async {
+    await db.execute(
+      "CREATE TABLE $tasksTable($colId INTEGER PRIMARY KEY AUTOINCREMENT, $colTitle TEXT, $colDate TEXT, $colPriority TEXT, $colStatus INTEGER",
+    );
+  }
+
+  Future<List<Map<String, dynamic>>> getTaskMapList() async {
+    Database db = await this.db;
+    final List<Map<String, dynamic>> result = await db.query(tasksTable);
+    return result;
+  }
+
+  Future<List<Task>> getTaskList() async {
+    final List<Map<String, dynamic>> taskMapList = await getTaskMapList();
+    final List<Task> taskList = [];
+    taskMapList.forEach((taskMap) {
+      taskList.add(Task.fromMap(taskMap));
+    });
+    return taskList;
+   }
+}
